3728ed07c9211037c564557974021980b427466e
Mohamad amin <145129471+itsSheesh@users.noreply.github.com>
2024-03-26 15:16:57 +0330

    fixing socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://gap-s4f8.onrender.com"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
